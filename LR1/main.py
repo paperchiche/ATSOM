c474b55acad02a4ef149dd75a6267ec3ff10499f
paperchiche <20artyom02@mail.ru>
2023-09-12 19:19:45 +0300

    Выполнение заданий №1, №2

---
+++ LR1/main.py
@@ -0,0 +1,18 @@
+import cv2
+# read image
+image = cv2.imread(r'C:/Users/20art/Desktop/01.jpg', cv2.IMREAD_COLOR)
+image2 = cv2.imread(r'C:/Users/20art/Desktop/01.jpg', cv2.IMREAD_LOAD_GDAL)
+image3 = cv2.imread(r'C:/Users/20art/Desktop/01.jpg', cv2.IMREAD_GRAYSCALE)
+# show image
+cv2.namedWindow('Display Window', cv2.WINDOW_NORMAL)
+cv2.imshow("Display Window", image)
+cv2.namedWindow('Display', cv2.WINDOW_AUTOSIZE)
+cv2.imshow("Display", image2)
+cv2.namedWindow('Display1', cv2.WINDOW_FULLSCREEN)
+cv2.imshow("Display1", image3)
+flags = cv2.IMREAD_COLOR
+# exit at closing of window
+
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
