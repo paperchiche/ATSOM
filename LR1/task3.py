a9e14ab2bd1df873e6f0cdb9cd0925a926166586
paperchiche <20artyom02@mail.ru>
2023-09-12 22:46:21 +0300

    Начало работы над заданием №4

--- LR1/task3.py
+++ LR1/task3.py
@@ -1,3 +1,7 @@
+# Задание 3. Отобразить видео в окне. Рассмотреть методы класса
+# VideoCapture и попробовать отображать видео в разных форматах, в частности
+# размеры и цветовая гамма.
+
 import cv2
 
 cap = cv2.VideoCapture(r'C:\Users\20art\Videos\Captures\video1.mp4')
@@ -11,6 +15,9 @@ cap = cv2.VideoCapture(r'C:\Users\20art\Videos\Captures\video1.mp4')
 
 cap.set(cv2.CAP_PROP_BRIGHTNESS, 0.5)
 cap.set(cv2.CAP_PROP_CONTRAST, 0.8)
+
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 300)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 300)
 
 while True:
     # Захват кадра из видеопотока.
