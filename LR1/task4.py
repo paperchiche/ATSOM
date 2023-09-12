9354ea2635e7bb1e877a4febe31df5779123b63b
paperchiche <20artyom02@mail.ru>
2023-09-12 23:55:14 +0300

    Проверка работы веб камеры

--- LR1/task4.py
+++ LR1/task4.py
@@ -1,1 +1,16 @@
-# Задание 4. Записать видео из файла в другой файл.
+import numpy as np
+import cv2
+
+capture = cv2.VideoCapture(0)
+
+while True:
+    ret, img = capture.read()
+
+    cv2.imshow("From Camera", img)
+
+    k = cv2.waitKey(30) & 0xFF
+    if k == 27:
+        break
+
+capture.release()
+cv2.destroyAllWindows()
