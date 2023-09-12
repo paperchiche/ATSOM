c0c198a82c43a1a672b85ef7f998e14cd7d46a2b
paperchiche <20artyom02@mail.ru>
2023-09-13 00:48:41 +0300

    Добавление эффекта размытия в задании №6

--- LR1/task6.py
+++ LR1/task6.py
@@ -27,6 +27,10 @@ bottom_right_x = top_left_x + rect_width
 bottom_right_y = top_left_y + rect_height
 cv2.rectangle(img, (top_left_x, top_left_y), (bottom_right_x, bottom_right_y), (0, 0, 255), 3)
 
+ROI = img[top_left_y:top_left_y+rect_height, top_left_x:top_left_x+rect_width]
+blur = cv2.GaussianBlur(ROI, (21, 21), 0)
+img[top_left_y:top_left_y+rect_height, top_left_x:top_left_x+rect_width] = blur
+
 cv2.namedWindow('image', cv2.WINDOW_NORMAL)  # Окно с изменяемым размером
 cv2.imshow('image', img)
 cv2.waitKey(0)
