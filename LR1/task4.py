60f7a71c790e43751056d93f917fbdf08b753753
paperchiche <20artyom02@mail.ru>
2023-09-13 00:02:01 +0300

    Задание №4. Запись видео с веб камеры в формате .avi

--- LR1/task4.py
+++ LR1/task4.py
@@ -1,16 +1,41 @@
-import numpy as np
 import cv2
 
-capture = cv2.VideoCapture(0)
+
+output_file = "output_video.avi"  # Имя выходного файла
+frame_width = 640  # Ширина кадра
+frame_height = 480  # Высота кадра
+fps = 30.0  # Количество кадров в секунду
+
+
+cap = cv2.VideoCapture(0)
+
+
+if not cap.isOpened():
+    print("Ошибка при открытии вебкамеры.")
+    exit()
+
+
+fourcc = cv2.VideoWriter_fourcc(*'XVID')  # кодек (XVID для AVI)
+out = cv2.VideoWriter(output_file, fourcc, fps, (frame_width, frame_height))
 
 while True:
-    ret, img = capture.read()
+    ret, frame = cap.read()
 
-    cv2.imshow("From Camera", img)
-
-    k = cv2.waitKey(30) & 0xFF
-    if k == 27:
+    if not ret:
+        print("Ошибка при захвате кадра.")
         break
 
-capture.release()
+    # Запись кадра в выходное видео
+    out.write(frame)
+
+    # зеркало
+    cv2.imshow("Webcam Video", frame)
+
+    # выход
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# выход
+cap.release()
+out.release()
 cv2.destroyAllWindows()
