1c958ff08c21ff1d9903fd9192792845d0ddd4c2
paperchiche <20artyom02@mail.ru>
2023-11-24 00:51:15 +0300

    Задание №1.8 - пентаграмма

--- LR1/task8.py
+++ LR1/task8.py
@@ -1,63 +1,30 @@
+# -*- coding: cp1251 -*-
 import math
-
 import cv2
-import numpy as np
+
+def draw_star(image, middle, size, angle_degrees):
+    points = []
+
+    for i in range(5):
+        x = int(middle[0] + size * math.cos(math.radians(i * 72 + angle_degrees)))
+        y = int(middle[1] + size * math.sin(math.radians(i * 72 + angle_degrees)))
+        points.append((x, y))
+
+    for i in range(5):
+        cv2.line(image, points[i], points[(i + 2) % 5], (0, 0, 0), 4)
+
+    cv2.circle(image, middle, size, (0, 0, 0), 4)
 
 img = cv2.imread(r'C:/Users/20art/Desktop/01.jpg')
-height, width, _ = img.shape
-center_x = width // 2
-center_y = height // 2
-# center_pixel = img[center_x, center_y]
 
-(b, g, r) = img[np.int16(center_y), np.int16(center_x)]
-print("Color at center pixel is - Red: {}, Green: {}, Blue: {}".format(r, g, b))
+# Координаты центра изображения
+middle = (img.shape[1] // 2, img.shape[0] // 2)
 
-def closest_color(x,y):
-    colors = {"0": (0, 0, 255), "1": (0, 255, 0), "2": (255, 0, 0)}
-    min_distance = float('inf')
-    closest = None
-    (b, g, r) = img[np.int16(y), np.int16(x)]
-    for color_name, color_value in colors.items():
-        distance = sum(((r,g,b) - np.array(color_value)) ** 2)
-        if math.sqrt(distance) < min_distance:
-            min_distance = math.sqrt(distance)
-            closest = color_name
+star_size = 100  # Размер пятиконечной звезды
+star_angle = 54
 
-    return closest
+draw_star(img, middle, star_size, star_angle)
 
-
-closest = closest_color(center_x, center_y)
-print(closest)
-
-rect_width = 50
-rect_height = 250
-
-top_left_x = (width - rect_width) // 2
-top_left_y = (height - rect_height) // 2
-bottom_right_x = top_left_x + rect_width
-bottom_right_y = top_left_y + rect_height
-if closest == "0":
-    cv2.rectangle(img, (top_left_x, top_left_y), (bottom_right_x, bottom_right_y), (255, 0, 0), -1)
-if closest == "1":
-    cv2.rectangle(img, (top_left_x, top_left_y), (bottom_right_x, bottom_right_y), (0, 255, 0), -1)
-if closest == "2":
-    cv2.rectangle(img, (top_left_x, top_left_y), (bottom_right_x, bottom_right_y), (0, 0, 255), -1)
-
-rect_width = 250
-rect_height = 50
-
-top_left_x = (width - rect_width) // 2
-top_left_y = (height - rect_height) // 2
-bottom_right_x = top_left_x + rect_width
-bottom_right_y = top_left_y + rect_height
-if closest == "0":
-    cv2.rectangle(img, (top_left_x, top_left_y), (bottom_right_x, bottom_right_y), (255, 0, 0), -1)
-if closest == "1":
-    cv2.rectangle(img, (top_left_x, top_left_y), (bottom_right_x, bottom_right_y), (0, 255, 0), -1)
-if closest == "2":
-    cv2.rectangle(img, (top_left_x, top_left_y), (bottom_right_x, bottom_right_y), (0, 0, 255), -1)
-
-cv2.namedWindow('image', cv2.WINDOW_NORMAL)
-cv2.imshow('image', img)
+cv2.imshow('Image', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
--- LR2/task1.py
+++ LR2/task1.py
@@ -14,7 +14,6 @@ while True:
         print("Can't read the frame")
         break
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    cv2.imshow('rgb', frame)
     cv2.imshow('hsv', hsv)
 
 cap.release()
