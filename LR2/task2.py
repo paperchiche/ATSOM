7ec3a2b1b45abcba2a81985d4d60570925973b36
paperchiche <20artyom02@mail.ru>
2023-11-24 19:04:17 +0300

    Задание №2.2 - изменение параметров

--- LR2/task2.py
+++ LR2/task2.py
@@ -7,18 +7,15 @@ cap = cv2.VideoCapture(0)
 
 while True:
     ret, frame = cap.read()
-
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    # определение диапазона красного цвета в HSV
-    min_red = np.array([0, 100, 0])
-    max_red = np.array([10, 255, 255])
+    lower_red = np.array([0, 120, 120])
+    upper_red = np.array([100, 255, 255])
 
-    mask = cv2.inRange(hsv, min_red, max_red)
-    res = cv2.bitwise_and(frame, frame, mask=mask)
+    mask = cv2.inRange(hsv, lower_red, upper_red)
+    onlyRed_frame = cv2.bitwise_and(frame, frame, mask = mask)
 
-    cv2.imshow('HSV_video', hsv)
-    cv2.imshow('Result_video', res)
+    cv2.imshow('Red Filtered Image', onlyRed_frame)
 
     if cv2.waitKey(1) & 0xFF == 27:
         break
