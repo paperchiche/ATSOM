cd9228a6c75a6075e38621de4fe9eac2f0cb4903
paperchiche <20artyom02@mail.ru>
2023-12-02 21:24:11 +0300

    Задание №4.1 - чтение и вывод изображение в чб + размытие

---
+++ LR4/main.py
@@ -0,0 +1,22 @@
+# -*- coding: cp1251 -*-
+import cv2
+
+
+def run():
+    img_original = cv2.imread(r'C:/Users/20art/Desktop/01.jpg', cv2.IMREAD_ANYCOLOR)
+    img = cv2.imread(r'C:/Users/20art/Desktop/01.jpg', cv2.IMREAD_GRAYSCALE)
+    img = cv2.resize(img, (450, 300))
+    img_original = cv2.resize(img_original, (450, 300))
+    cv2.imshow('original', img_original)
+
+    cv2.imshow('grey', img)
+
+    blur_opencv = cv2.GaussianBlur(img, (3, 3), 2)
+    cv2.imshow('opencv blur', blur_opencv)
+    cv2.moveWindow('grey', 600, 100)
+    cv2.moveWindow('opencv blur', 300, 400)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+run()
