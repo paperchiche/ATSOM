c4bec25ca3cbde7d4d8543b07d5ba91212b0fda9
paperchiche <20artyom02@mail.ru>
2023-12-10 22:32:37 +0300

    ЛР6 - сохранение модели cnn

--- LR6/Convolutional_Neural_Network.py
+++ LR6/Convolutional_Neural_Network.py
@@ -2,8 +2,11 @@
 import tensorflow as tf
 from tensorflow.keras import datasets, layers, models
 import time
+from torch.utils.tensorboard import SummaryWriter
+import numpy as np
 
 start_time = time.time()
+writer = SummaryWriter(log_dir='/LR_6/logs_2')
 
 # загрузка данных MNIST (первый кортеж - тренировочные изображения и метки, а второй - тестовые изображения и метки)
 (train_images, train_labels), (test_images, test_labels) = datasets.mnist.load_data()
@@ -33,6 +36,7 @@ model.add(layers.Flatten())
 model.add(layers.Dense(64, activation='relu'))
 model.add(layers.Dense(10))
 
+# компиляция модели и обучение ее на тренировочных данных
 # настройка процесса обучения модели
 model.compile(optimizer='adam',
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
@@ -41,11 +45,20 @@ model.compile(optimizer='adam',
 # # Добавление TensorBoard в модель
 tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir="C:\GitHub\ATSOM")
 
+# запись данные изображений в файлы событий
+images = train_images[:20].reshape(-1, 28, 28, 1)
+images = (images * 255).astype(np.uint8)
+writer.add_images('mnist_images', images, dataformats='NHWC')
+writer.close()
+
 # обучение модели на тренировочных данных
 history = model.fit(train_images, train_labels,
                     epochs=5,
                     validation_data=(test_images, test_labels),
                     callbacks=[tensorboard_callback])
+
+# сохранение модели
+model.save("cnn_model.keras")
 
 print('==============================================================')
 # оценка потерь и точности модели
