278bb7672e371b8bc4134a7dd609fc0dd1e43546
paperchiche <20artyom02@mail.ru>
2023-12-12 20:11:17 +0300

    Задание №5.1 - вывод видео с движением

--- LR5/main.py
+++ LR5/main.py
@@ -1,74 +1,44 @@
-# -*- coding: cp1251 -*-
 import cv2
 import numpy as np
 
-i = 0
-def main(kernel_size, standard_deviation, delta_tresh, min_area):
-    global i
-    i += 1
+video = cv2.VideoCapture("main_video.mov", cv2.CAP_ANY)
 
-    video = cv2.VideoCapture('main_video.mov', cv2.CAP_ANY)
 
-    ret, frame = video.read()
+def detect(kernel_size, standard_deviation, delta_tresh, min_area):
+
+    _, frame = video.read()
     img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    img = cv2.GaussianBlur(img, (kernel_size, kernel_size), standard_deviation)
+    img = cv2.GaussianBlur(
+        img, (kernel_size, kernel_size), standard_deviation)
 
-    w = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
-    h = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    w = int(video.get(3))
+    h = int(video.get(4))
+
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-    video_writer = cv2.VideoWriter('result_videos/result' + str(i) + '.mp4', fourcc, 144, (w, h))
+    video_writer = cv2.VideoWriter('output.mp4', fourcc, 25, (w, h))
 
     while True:
-        # сохраняем старый кадр, чтобы вычислить разниц между кадрами
         old_img = img.copy()
-        ok, frame = video.read()
-        if not ok:
+        flag, frame = video.read()
+        if not flag:
             break
-
         img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        img = cv2.GaussianBlur(img, (kernel_size, kernel_size), standard_deviation)
+        img = cv2.GaussianBlur(
+            img, (kernel_size, kernel_size), standard_deviation)
 
-        # вычисляем разницу
         diff = cv2.absdiff(img, old_img)
-        # бинаризируем её, превращая пиксели, превышающие порог delta_tresh, в белый цвет, а остальные в черный
-        # сохраняем только пороговое значение
         thresh = cv2.threshold(diff, delta_tresh, 255, cv2.THRESH_BINARY)[1]
-        # print(thresh)
-        # cv2.imshow("a",thresh)
-        # находим контуры
-        (contors, hierarchy) = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-        # если на кадре есть хотя бы один контур, чья площадь достаточно большая, то записываем кадр
+        contors, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         for contr in contors:
             area = cv2.contourArea(contr)
-            if area < min_area:
-                continue
-            video_writer.write(frame)
-
+            if area >= min_area:
+                video_writer.write(frame)
+                break
     video_writer.release()
 
 
-kernel_size = 3
-standard_deviation = 50
-delta_tresh = 60
-min_area = 20
-main(kernel_size, standard_deviation, delta_tresh, min_area)
-
-# оптимальный вариант
-kernel_size = 11
-standard_deviation = 70
-delta_tresh = 60
-min_area = 20
-main(kernel_size, standard_deviation, delta_tresh, min_area)
-
-kernel_size = 3
-standard_deviation = 50
+kernel_size = 5
+standard_deviation = 30
 delta_tresh = 20
-min_area = 20
-main(kernel_size, standard_deviation, delta_tresh, min_area)
-
-kernel_size = 3
-standard_deviation = 50
-delta_tresh = 60
-min_area = 10
-main(kernel_size, standard_deviation, delta_tresh, min_area)
+min_area = 50
+detect(kernel_size, standard_deviation, delta_tresh, min_area)
